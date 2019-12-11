9a1614f2f79255f795d58fd339d79b65ed37160e
Wall <307606056@qq.com>
2019-12-11 21:07:04 +0800

    数据库基本操作测试

--- src/renderer/datastore.js
+++ src/renderer/datastore.js
@@ -1,8 +1,27 @@
 import Datastore from 'nedb'
 import path from 'path'
-import { remote } from 'electron'
+import {remote} from 'electron'
 
-export default new Datastore({
-    autoload: true,
-    filename: path.join(remote.app.getPath('userData'), '/data.db')
-})
+const db = {};
+db.test = new Datastore(path.join(remote.app.getPath('userData'), '/test.db'));
+db.test.loadDatabase();
+
+db.user = new Datastore(path.join(remote.app.getPath('userData'), '/user.db'));
+db.user.loadDatabase();
+
+db.item = new Datastore(path.join(remote.app.getPath('userData'), '/item.db'));
+db.item.loadDatabase();
+
+db.itemCat = new Datastore(path.join(remote.app.getPath('userData'), '/itemCat.db'));
+db.itemCat.loadDatabase();
+
+db.order = new Datastore(path.join(remote.app.getPath('userData'), '/order.db'));
+db.order.loadDatabase();
+
+db.payment = new Datastore(path.join(remote.app.getPath('userData'), '/payment.db'));
+db.payment.loadDatabase();
+
+db.stock = new Datastore(path.join(remote.app.getPath('userData'), '/stock.db'));
+db.stock.loadDatabase();
+
+export default db;
