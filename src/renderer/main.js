e5f7c32103ed9d1085ccaae3b08b93cfa320d0e7
Wall <307606056@qq.com>
2019-12-11 17:56:33 +0800

    数据库嵌入

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
-import ElementUI from 'element-ui'
+import ElementUI from 'element-ui' // 添加ElementUI
 import 'element-ui/lib/theme-chalk/index.css'
 import db from './datastore' // 添加本地数据库
 
@@ -12,7 +12,7 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 
-Vue.use(ElementUI);
+Vue.use(ElementUI); // 添加ElementUI
 
 Vue.prototype.$db = db; // 添加本地数据库
 
