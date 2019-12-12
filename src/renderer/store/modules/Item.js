99f9836d736250775580820c66a6a8c0cd86f375
Wall <307606056@qq.com>
2019-12-12 14:57:39 +0800

    数据库操作及vuex交互

--- src/renderer/store/modules/Item.js
+++ src/renderer/store/modules/Item.js
@@ -13,26 +13,15 @@ const mutations = {
     GET_ITEM(state, payload) {
         state.list = payload
     },
-    CREATE_ITEM(state) {
-        db.item.find({}, (err, docs) => {
-            state.data = docs
-        });
-    },
-    DELETE_ITEM(state) {
-        db.item.remove({}, (err, docs) => {
-            state.data = docs
-        });
-    },
 };
 
 const actions = {
     deleteAllItem({dispatch}) {
         db.item.remove({}, {multi: true}, (err, newDocs) => {
-            console.log(newDocs);
             dispatch("getItem")
         })
     },
-    createItem({dispatch, commit}) {
+    createItem({dispatch}) {
         let doc = {name: "123"};
         db.item.insert(doc, (err, newDocs) => {
             dispatch("getItem")
@@ -40,14 +29,9 @@ const actions = {
     },
     getItem({commit}) {
         db.item.find({}, (err, docs) => {
-            console.log(docs)
             commit('GET_ITEM', docs);
         });
     },
-    someAsyncTask({commit}) {
-        // do something async
-        commit('INCREMENT_MAIN_COUNTER')
-    }
 };
 
 export default {
