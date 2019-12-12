8d525aa87e71d8442285f398d972cfe462ac390e
Wall <307606056@qq.com>
2019-12-12 19:21:45 +0800

    收银台挂单提单方法

--- src/renderer/store/modules/Cashier.js
+++ src/renderer/store/modules/Cashier.js
@@ -1,4 +1,4 @@
-const state = {
+const defaultState = {
     order: {
         type: undefined,
         consumer: {
@@ -14,39 +14,85 @@ const state = {
     cacheOrder: [],
 };
 
+
+const state = Object.assign({}, defaultState);
+
 const mutations = {
-    SET_ORDER(state, payload) {
-        db.item.find({}, (err, docs) => {
-            state.data = docs
-        });
+    // 清空收银台
+    ClEAR_ALL(state) {
+        state = defaultState;
     },
 
+    // 清空订单
+    ClEAR_ORDER(state) {
+        state.order = defaultState.order
+    },
+    CLEAN_SUBORDER(state) {
+        state.order.subOrder = []
+    },
+
+    // 添加商品
+    CREATE_CASHIER_GOODS(state, payload) {
+        const index = state.order.subOrder.findIndex((v) => (v._id === payload._id));
+        if (index !== -1) {
+            state.order.subOrder[index].quantity += 1;
+        } else {
+            state.order.subOrder.push(payload);
+        }
+        state.cacheOrder.push(state.order);
+    },
+    // 删除商品
+    DELETE_CASHIER_GOODS(state, payload) {
+        const index = state.order.subOrder.findIndex((v) => (v._id === payload._id));
+        if (index !== -1) {
+            state.order.subOrder[index].quantity += 1;
+        } else {
+            state.order.subOrder.push(payload);
+        }
+        state.cacheOrder.push(state.order);
+    },
+    // 挂单
     SET_CACHE_ORDER(state) {
         state.cacheOrder.push(state.order);
     },
-
+    // 提单
     GET_CACHE_ORDER(state, payload) {
         state.order = state.cacheOrder.find((value, index) => index === payload);
     },
+    // 挂单删除
     DELETE_CACHE_ORDER(state, payload) {
         state.cacheOrder = state.cacheOrder.filter((value, index) => index !== payload)
     },
 };
 
 const actions = {
-    // 添加商品
-    clearOrder(ctx) {
+    // 清空全部
+    clearAll(ctx) {
+        ctx.commit("ClEAR_ALL")
     },
 
-    // 添加商品
-    createSubOrder(ctx, payload) {
-    },
-    // 删除商品
-    deleteSubOrder(ctx, payload) {
+    // 清空order
+    clearOrder(ctx) {
+        ctx.commit("ClEAR_ORDER")
     },
     // 清空商品
     cleanSubOrder(ctx, payload) {
+        ctx.commit("CLEAN_SUBORDER")
     },
+    // 修改商品数量
+    UpdateGoodsQuantity(ctx, payload) {
+        ctx.commit("UPDATE_GOODS_QUANTITY")
+    },
+
+    // 添加商品
+    createCashierGoods(ctx, payload) {
+        ctx.commit("CREATE_CASHIER_GOODS")
+    },
+    // 删除商品
+    deleteSubOrder(ctx, payload) {
+        ctx.commit("DELETE_CASHIER_GOODS")
+    },
+
     // 挂单
     setCacheOrder(ctx) {
         ctx.commit("SET_CACHE_ORDER")
