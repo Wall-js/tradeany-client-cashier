4c60dfd08c33f5cc5e2b5e13071a121adb4592ef
Wall <307606056@qq.com>
2019-12-12 14:42:22 +0800

    数据库操作及vuex交互

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -67,6 +67,12 @@
             icon:'el-icon-notebook-2',
             index: "/printer"
           },
+            {
+                type: 'menuItem',
+                title: 'DEMO',
+                icon: 'el-icon-notebook-2',
+                index: "/demo"
+            },
         ],
         userName:'admin',
         isShow:true
--- src/renderer/store/modules/Item.js
+++ src/renderer/store/modules/Item.js
@@ -10,17 +10,15 @@ const state = {
 };
 
 const mutations = {
-    GET(state) {
+    GET_ITEM(state, payload) {
+        state.list = payload
+    },
+    CREATE_ITEM(state) {
         db.item.find({}, (err, docs) => {
             state.data = docs
         });
     },
-    CREATE(state) {
-        db.item.find({}, (err, docs) => {
-            state.data = docs
-        });
-    },
-    DELETE(state) {
+    DELETE_ITEM(state) {
         db.item.remove({}, (err, docs) => {
             state.data = docs
         });
@@ -28,9 +26,23 @@ const mutations = {
 };
 
 const actions = {
-    get({commit}) {
-        // do something async
-        commit('INCREMENT_MAIN_COUNTER')
+    deleteAllItem({dispatch}) {
+        db.item.remove({}, {multi: true}, (err, newDocs) => {
+            console.log(newDocs);
+            dispatch("getItem")
+        })
+    },
+    createItem({dispatch, commit}) {
+        let doc = {name: "123"};
+        db.item.insert(doc, (err, newDocs) => {
+            dispatch("getItem")
+        })
+    },
+    getItem({commit}) {
+        db.item.find({}, (err, docs) => {
+            console.log(docs)
+            commit('GET_ITEM', docs);
+        });
     },
     someAsyncTask({commit}) {
         // do something async
